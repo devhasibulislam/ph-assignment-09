bb9f433bab7c22fffd11885f7f78a246439eb392
hasibulislam999 <hasib143sl@gmail.com>
2022-04-04 17:48:58 +0600

    qna border fix

--- src/components/QNACards/QNACards.js
+++ src/components/QNACards/QNACards.js
@@ -3,7 +3,7 @@ import React from 'react';
 const QNACards = ({ qa }) => {
     const { answer, question } = qa;
     return (
-        <div className='mb-8 border-l-2 border-t-2 p-4'>
+        <div className='mb-8 border-l-2 border-t-2 p-4 rounded-lg'>
             <h3 className='text-2xl font-bold mb-2'>{question}</h3>
             <p>{answer}</p>
         </div>
